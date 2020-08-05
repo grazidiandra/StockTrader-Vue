89d38ff413efadd9d1ea4ba1df2317bd97aa668c
grazidiandra <grazi.diandra@gmail.com>
2020-08-05 11:32:23 -0300

    Components, routes

---
+++ src/components/stocks/Stock.vue
@@ -0,0 +1,47 @@
+<template>
+  <v-flex class="pr-3 pb-3" xs12 md6 lg4>
+    <v-card class="green darken-3 white--text">
+      <v-card-title class="headline">
+        <strong>{{ stock.name }}<small> (preço: {{ stock.price | price }})</small></strong>
+      </v-card-title>
+    </v-card>
+    <v-card>
+      <v-container fill-height>
+        <v-text-field label="Quantidade" type="number" v-model.number="quantity"/>
+        <v-btn class="green darken-3 white--text" :disabled="quantity <= 0 || !Number.isInteger(quantity)" @click="buyStock">Comprar</v-btn>
+      </v-container>
+    </v-card>
+  </v-flex>
+</template>
+
+<script>
+export default {
+  props: ['stock'],
+  data() {
+    return {
+      quantity: 0
+    }
+  },
+  filters: {
+    price(value) {
+      return `R$ ${parseFloat(value).toFixed(2)}`.replace('.', ',')
+    }
+  },
+  methods: {
+    buyStock() {
+
+      const order = {
+        idStock: this.stock.id,
+        priceStock: this.stock.price,
+        quantity: this.quantity
+      }
+      console.log(order)
+      this.quantity = 0
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/stocks/Stocks.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-layout row wrap>
+    <Stock v-for="stock in stocks" :key="stock.id" :stock="stock"/>
+  </v-layout>
+</template>
+
+<script>
+import Stock from './Stock'
+
+export default {
+  components:{ Stock },
+  data() {
+    return {
+      stocks: [
+        {id: 1, name: 'BMW', price: 110},
+        {id: 2, name: 'Google', price: 230},
+        {id: 3, name: 'Apple', price: 270},
+        {id: 4, name: 'Twitter', price: 93},
+      ]
+
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
